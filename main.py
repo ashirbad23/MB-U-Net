20f4148327573aa68b466ee7763094a3d34f3c27
ashirbad23 <paridaashirbad23@gmail.com>
2026-03-30 18:37:58 +0530

    feat: add main entry point with config, train runner, and gitignore updates

---
+++ main.py
@@ -0,0 +1,54 @@
+import torch
+from pathlib import Path
+from src.train.train import train
+import os
+
+ROOT = Path(__file__).resolve().parent
+
+DATASET = ROOT / "dataset"
+CONFIG = ROOT / "config"
+CHECKPOINTS = ROOT / "model_checkpoints"
+
+model_config = {
+    "mode": "train",
+    # ================= PATHS =================
+    "dataset": str(ROOT / "dataset"),
+    "split_path": str(ROOT / "config" / "train_val_split.json"),
+    "checkpoint": str(ROOT / "model_checkpoints" / "exp_v1"),
+
+    # ================= DEVICE =================
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+
+    # ================= DATA =================
+    "patch_size": 64,
+    "overlap_train": 0.5,
+    "overlap_val": 1,
+
+    # ================= BANDS =================
+    "bands_used": None,
+
+    # ================= MODEL =================
+    "in_channels": 18,
+    "out_channels": 1,
+    "channel_head": 128,
+    "num_res_blocks": 2,
+    "dropout": 0.1,
+    "use_attention": False,
+    "use_se": True,
+
+    # ================= TRAINING =================
+    "batch_size": 4,
+    "num_workers": 2,
+    "epochs": 10,
+
+    # ================= OPTIMIZATION =================
+    "learning_rate": 1e-4,
+    "warmup_multiplier": 2.5,
+
+    # ================= LOSS =================
+    "num_classes": 2
+}
+
+if __name__ == "__main__":
+    if model_config["mode"] == "train":
+        train(model_config)
