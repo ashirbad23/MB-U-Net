857ece47b77d07fa6493af390e278e2bd004dd08
ashirbad23 <paridaashirbad23@gmail.com>
2026-04-05 10:31:26 +0530

    fix: using get_lr

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -12,28 +12,21 @@ class GradualWarmupScheduler(LRScheduler):
         super().__init__(optimizer)
 
     def get_lr(self):
-        if self.last_epoch >= self.total_epoch:
+        if self.last_epoch > self.total_epoch:
             if self.after_scheduler:
                 if not self.finished:
-                    self.after_scheduler.base_lrs = [
-                        base_lr * self.multiplier for base_lr in self.base_lrs
-                    ]
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
                     self.finished = True
-                return self.after_scheduler.get_last_lr()
+                return self.after_scheduler.get_lr()
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
-
-        return [
-            base_lr * (
-                    1.0 + (self.multiplier - 1.0) * self.last_epoch / self.total_epoch
-            )
-            for base_lr in self.base_lrs
-        ]
+        return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in
+                self.base_lrs]
 
     def step(self, epoch=None, metrics=None):
         if self.finished and self.after_scheduler:
             if epoch is None:
                 self.after_scheduler.step(None)
             else:
-                self.after_scheduler.step(epoch - self.total_epoch + 1)
+                self.after_scheduler.step(epoch - self.total_epoch)
         else:
-            return super().step(epoch)
+            return super(GradualWarmupScheduler, self).step(epoch)
