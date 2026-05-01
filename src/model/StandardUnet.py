6915b9aaba0d8e4b83c8d89e92e5748aa25e0145
SmarakOta <smarak.ota@gmail.com>
2026-05-01 20:54:10 +0530

    Updated model

--- src/model/StandardUnet.py
+++ src/model/StandardUnet.py
@@ -9,14 +9,36 @@ class Swish(nn.Module):
         return x * torch.sigmoid(x)
 
 
+class AttnBlock(nn.Module):
+    def __init__(self, in_ch):
+        super().__init__()
+        self.group_norm = nn.GroupNorm(16, in_ch)
+        self.proj_q = nn.Conv2d(in_ch, in_ch, 1)
+        self.proj_k = nn.Conv2d(in_ch, in_ch, 1)
+        self.proj_v = nn.Conv2d(in_ch, in_ch, 1)
+        self.proj = nn.Conv2d(in_ch, in_ch, 1)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        h = self.group_norm(x)
+        q = self.proj_q(h).permute(0, 2, 3, 1).reshape(B, H * W, C)
+        k = self.proj_k(h).reshape(B, C, H * W)
+        w = torch.bmm(q, k) * (C ** -0.5)
+        w = F.softmax(w, dim=-1)
+
+        v = self.proj_v(h).permute(0, 2, 3, 1).reshape(B, H * W, C)
+        h = torch.bmm(w, v).reshape(B, H, W, C).permute(0, 3, 1, 2)
+        h = self.proj(h)
+
+        return x + h
+
+
 class SEBlock(nn.Module):
     def __init__(self, in_ch, reduction=2):
         super().__init__()
-
         hidden = max(in_ch // reduction, 1)
 
         self.pool = nn.AdaptiveAvgPool2d(1)
-
         self.fc = nn.Sequential(
             nn.Linear(in_ch, hidden, bias=False),
             nn.ReLU(inplace=True),
@@ -25,11 +47,9 @@ class SEBlock(nn.Module):
         )
 
     def forward(self, x):
-        B, C, H, W = x.shape
-
+        B, C, _, _ = x.shape
         y = self.pool(x).view(B, C)
         y = self.fc(y).view(B, C, 1, 1)
-
         return x * y, y
 
 
@@ -39,33 +59,32 @@ class IdentitySE(nn.Module):
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, dropout):
+    def __init__(self, in_ch, out_ch, dropout, attn=False):
         super().__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.InstanceNorm2d(out_ch),
             Swish(),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.InstanceNorm2d(out_ch),
             nn.Dropout2d(dropout)
         )
 
-        if in_ch != out_ch:
-            self.shortcut = nn.Conv2d(in_ch, out_ch, kernel_size=1)
-        else:
-            self.shortcut = nn.Identity()
-
+        self.shortcut = nn.Conv2d(in_ch, out_ch, 1) if in_ch != out_ch else nn.Identity()
+        self.attn = AttnBlock(out_ch) if attn else nn.Identity()
         self.swish = Swish()
 
     def forward(self, x):
-        return self.swish(self.block(x) + self.shortcut(x))
+        h = self.block(x) + self.shortcut(x)
+        h = self.attn(h)
+        return self.swish(h)
 
 
 class DownBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, dropout):
+    def __init__(self, in_ch, out_ch, dropout, attn=False):
         super().__init__()
-        self.res = ResBlock(in_ch, out_ch, dropout)
+        self.res = ResBlock(in_ch, out_ch, dropout, attn)
         self.pool = nn.MaxPool2d(2)
 
     def forward(self, x):
@@ -75,57 +94,61 @@ class DownBlock(nn.Module):
 
 
 class UpBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, dropout):
+    def __init__(self, in_ch, out_ch, dropout, attn=False):
         super().__init__()
         self.up = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
-        self.res = ResBlock(in_ch, out_ch, dropout)
+        self.res = ResBlock(in_ch, out_ch, dropout, attn)
 
     def forward(self, x, skip):
         x = self.up(x)
+
+        if x.shape[-2:] != skip.shape[-2:]:
+            x = F.interpolate(x, size=skip.shape[-2:], mode='bilinear', align_corners=False)
+
         x = torch.cat([x, skip], dim=1)
         return self.res(x)
 
 
 class SUnet(nn.Module):
-    def __init__(self, ch_head, in_ch, out_ch, dropout, se=True):
+    def __init__(self, ch_head, in_ch, out_ch, dropout, attn=True, se=True):
         super().__init__()
 
-        self.head = nn.Conv2d(in_ch, ch_head, 3, 1, 1)
+        self.head = nn.Conv2d(in_ch, ch_head, 3, padding=1)
+
+        # channel multipliers
+        ch_mult = [1, 2, 4, 8]
+        chs = [ch_head * m for m in ch_mult]
 
         # Encoder
-        self.down1 = DownBlock(ch_head, 64, dropout)
-        self.down2 = DownBlock(64, 128, dropout)
-        self.down3 = DownBlock(128, 256, dropout)
-        self.down4 = DownBlock(256, 512, dropout)
+        self.down1 = DownBlock(ch_head, chs[0], dropout, attn)
+        self.down2 = DownBlock(chs[0], chs[1], dropout, attn)
+        self.down3 = DownBlock(chs[1], chs[2], dropout, attn)
+        self.down4 = DownBlock(chs[2], chs[3], dropout, attn)
 
         # Bottleneck
-        self.bottleneck = ResBlock(512, 1024, dropout)
+        self.bottleneck = ResBlock(chs[3], chs[3]*2, dropout, attn)
 
         # Decoder
-        self.up1 = UpBlock(1024, 512, dropout)
-        self.up2 = UpBlock(512, 256, dropout)
-        self.up3 = UpBlock(256, 128, dropout)
-        self.up4 = UpBlock(128, 64, dropout)
+        self.up1 = UpBlock(chs[3]*2, chs[3], dropout, attn)
+        self.up2 = UpBlock(chs[3], chs[2], dropout, attn)
+        self.up3 = UpBlock(chs[2], chs[1], dropout, attn)
+        self.up4 = UpBlock(chs[1], chs[0], dropout, attn)
 
         self.tail = nn.Sequential(
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.InstanceNorm2d(64),
+            nn.Conv2d(chs[0], chs[0], 3, padding=1),
+            nn.InstanceNorm2d(chs[0]),
             Swish(),
-            nn.Conv2d(64, out_ch, 1)
+            nn.Conv2d(chs[0], out_ch, 1)
         )
 
-        if se:
-            self.se = SEBlock(in_ch - 6)
-        else:
-            self.se = IdentitySE()
+        self.se = SEBlock(in_ch - 6) if se else IdentitySE()
 
     def forward(self, x):
         geo_weights = None
 
         if x.shape[1] > 6:
-            sat = x[:, :6, :, :]
-            geo = x[:, 6:, :, :]
-
+            sat = x[:, :6]
+            geo = x[:, 6:]
             geo, geo_weights = self.se(geo)
             x = torch.cat([sat, geo], dim=1)
 
@@ -153,7 +176,7 @@ if __name__ == "__main__":
 
     in_channels = 18
     out_channels = 1
-    ch_head = 64
+    ch_head = 32
     dropout = 0.1
 
     model = SUnet(
@@ -161,6 +184,7 @@ if __name__ == "__main__":
         in_ch=in_channels,
         out_ch=out_channels,
         dropout=dropout,
+        attn=True,
         se=True
     ).to(device)
 
